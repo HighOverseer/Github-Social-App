f9f1ba5c3080edfe953084efe5f78affe52f8049
Fajar Alif Riyandi <fajariyandi36@gmail.com>
2024-04-10 21:57:52 +0700

    setting credentials

---
+++ environmentSetup.sh
@@ -0,0 +1,15 @@
+function copyEnvVarsToGradleProperties{
+  GRADLE_PROPERTIES=$HOME"/gradle.properties"
+  export GRADLE_PROPERTIES
+  echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+  if [ ! -f "$GRADLE_PROPERTIES" ]; then
+    echo "Gradle Properties does not exist"
+
+    echo "Creating Gradle Properties file..."
+    touch $GRADLE_PROPERTIES
+
+    echo "Writing ORG_GRADLE_PROJECT_API_TOKEN to gradle.properties..."
+    echo "API_TOKEN=$ORG_GRADLE_PROJECT_API_TOKEN" >> $GRADLE_PROPERTIES
+  fi
+}
